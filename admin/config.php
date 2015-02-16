04616f5864826010502d262f9d902fe015737fdf
Walt Ellsworth <walt@photo-concepts.com>
2015-02-15 18:53:00 -0800

    moved files to dcs directory

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/admin/');
+define('HTTP_CATALOG', 'http://localhost:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/PATRIOT/Projects/DCS 2015/admin/');
+define('DIR_SYSTEM', '/Volumes/PATRIOT/Projects/DCS 2015/system/');
+define('DIR_LANGUAGE', '/Volumes/PATRIOT/Projects/DCS 2015/admin/language/');
+define('DIR_TEMPLATE', '/Volumes/PATRIOT/Projects/DCS 2015/admin/view/template/');
+define('DIR_CONFIG', '/Volumes/PATRIOT/Projects/DCS 2015/system/config/');
+define('DIR_IMAGE', '/Volumes/PATRIOT/Projects/DCS 2015/image/');
+define('DIR_CACHE', '/Volumes/PATRIOT/Projects/DCS 2015/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/download/');
+define('DIR_UPLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/upload/');
+define('DIR_LOGS', '/Volumes/PATRIOT/Projects/DCS 2015/system/logs/');
+define('DIR_MODIFICATION', '/Volumes/PATRIOT/Projects/DCS 2015/system/modification/');
+define('DIR_CATALOG', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'Dreamy_Craft_Shop');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/');
+define('DIR_SYSTEM', '/Volumes/PATRIOT/Projects/DCS 2015/system/');
+define('DIR_LANGUAGE', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/language/');
+define('DIR_TEMPLATE', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/view/theme/');
+define('DIR_CONFIG', '/Volumes/PATRIOT/Projects/DCS 2015/system/config/');
+define('DIR_IMAGE', '/Volumes/PATRIOT/Projects/DCS 2015/image/');
+define('DIR_CACHE', '/Volumes/PATRIOT/Projects/DCS 2015/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/download/');
+define('DIR_UPLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/upload/');
+define('DIR_MODIFICATION', '/Volumes/PATRIOT/Projects/DCS 2015/system/modification/');
+define('DIR_LOGS', '/Volumes/PATRIOT/Projects/DCS 2015/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'Dreamy_Craft_Shop');
+define('DB_PREFIX', 'oc_');
