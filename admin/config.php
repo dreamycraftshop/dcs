c53ad5674c249c9922ad01b1ded8bed664f8eaec
Walt Ellsworth <walt@photo-concepts.com>
2015-02-19 08:44:50 -0800

    added files to elitewebsources server

--- admin/config.php
+++ admin/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8888/admin/');
-define('HTTP_CATALOG', 'http://localhost:8888/');
+define('HTTP_SERVER', 'http://www.elitewebsources.com/admin/');
+define('HTTP_CATALOG', 'http://www.elitewebsources.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8888/admin/');
-define('HTTPS_CATALOG', 'http://localhost:8888/');
+define('HTTPS_SERVER', 'http://www.elitewebsources.com/admin/');
+define('HTTPS_CATALOG', 'http://www.elitewebsources.com/');
 
 // DIR
-define('DIR_APPLICATION', '/Volumes/PATRIOT/Projects/DCS 2015/admin/');
-define('DIR_SYSTEM', '/Volumes/PATRIOT/Projects/DCS 2015/system/');
-define('DIR_LANGUAGE', '/Volumes/PATRIOT/Projects/DCS 2015/admin/language/');
-define('DIR_TEMPLATE', '/Volumes/PATRIOT/Projects/DCS 2015/admin/view/template/');
-define('DIR_CONFIG', '/Volumes/PATRIOT/Projects/DCS 2015/system/config/');
-define('DIR_IMAGE', '/Volumes/PATRIOT/Projects/DCS 2015/image/');
-define('DIR_CACHE', '/Volumes/PATRIOT/Projects/DCS 2015/system/cache/');
-define('DIR_DOWNLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/download/');
-define('DIR_UPLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/upload/');
-define('DIR_LOGS', '/Volumes/PATRIOT/Projects/DCS 2015/system/logs/');
-define('DIR_MODIFICATION', '/Volumes/PATRIOT/Projects/DCS 2015/system/modification/');
-define('DIR_CATALOG', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/');
+define('DIR_APPLICATION', '/home2/hostsit1/public_html/elitewebsources/admin/');
+define('DIR_SYSTEM', '/home2/hostsit1/public_html/elitewebsources/system/');
+define('DIR_LANGUAGE', '/home2/hostsit1/public_html/elitewebsources/admin/language/');
+define('DIR_TEMPLATE', '/home2/hostsit1/public_html/elitewebsources/admin/view/template/');
+define('DIR_CONFIG', '/home2/hostsit1/public_html/elitewebsources/system/config/');
+define('DIR_IMAGE', '/home2/hostsit1/public_html/elitewebsources/image/');
+define('DIR_CACHE', '/home2/hostsit1/public_html/elitewebsources/system/cache/');
+define('DIR_DOWNLOAD', '/home2/hostsit1/public_html/elitewebsources/system/download/');
+define('DIR_UPLOAD', '/home2/hostsit1/public_html/elitewebsources/system/upload/');
+define('DIR_LOGS', '/home2/hostsit1/public_html/elitewebsources/system/logs/');
+define('DIR_MODIFICATION', '/home2/hostsit1/public_html/elitewebsources/system/modification/');
+define('DIR_CATALOG', '/home2/hostsit1/public_html/elitewebsources/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'Dreamy_Craft_Shop');
+define('DB_USERNAME', 'hostsit1_butch');
+define('DB_PASSWORD', '!5820Butch!');
+define('DB_DATABASE', 'hostsit1_DCS');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost:8888/');
+define('HTTP_SERVER', 'http://www.elitewebsources.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost:8888/');
+define('HTTPS_SERVER', 'http://www.elitewebsources.com/');
 
 // DIR
-define('DIR_APPLICATION', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/');
-define('DIR_SYSTEM', '/Volumes/PATRIOT/Projects/DCS 2015/system/');
-define('DIR_LANGUAGE', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/language/');
-define('DIR_TEMPLATE', '/Volumes/PATRIOT/Projects/DCS 2015/catalog/view/theme/');
-define('DIR_CONFIG', '/Volumes/PATRIOT/Projects/DCS 2015/system/config/');
-define('DIR_IMAGE', '/Volumes/PATRIOT/Projects/DCS 2015/image/');
-define('DIR_CACHE', '/Volumes/PATRIOT/Projects/DCS 2015/system/cache/');
-define('DIR_DOWNLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/download/');
-define('DIR_UPLOAD', '/Volumes/PATRIOT/Projects/DCS 2015/system/upload/');
-define('DIR_MODIFICATION', '/Volumes/PATRIOT/Projects/DCS 2015/system/modification/');
-define('DIR_LOGS', '/Volumes/PATRIOT/Projects/DCS 2015/system/logs/');
+define('DIR_APPLICATION', '/home2/hostsit1/public_html/elitewebsources/catalog/');
+define('DIR_SYSTEM', '/home2/hostsit1/public_html/elitewebsources/system/');
+define('DIR_LANGUAGE', '/home2/hostsit1/public_html/elitewebsources/catalog/language/');
+define('DIR_TEMPLATE', '/home2/hostsit1/public_html/elitewebsources/catalog/view/theme/');
+define('DIR_CONFIG', '/home2/hostsit1/public_html/elitewebsources/system/config/');
+define('DIR_IMAGE', '/home2/hostsit1/public_html/elitewebsources/image/');
+define('DIR_CACHE', '/home2/hostsit1/public_html/elitewebsources/system/cache/');
+define('DIR_DOWNLOAD', '/home2/hostsit1/public_html/elitewebsources/system/download/');
+define('DIR_UPLOAD', '/home2/hostsit1/public_html/elitewebsources/system/upload/');
+define('DIR_MODIFICATION', '/home2/hostsit1/public_html/elitewebsources/system/modification/');
+define('DIR_LOGS', '/home2/hostsit1/public_html/elitewebsources/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'Dreamy_Craft_Shop');
+define('DB_USERNAME', 'hostsit1_butch');
+define('DB_PASSWORD', '!5820Butch!');
+define('DB_DATABASE', 'hostsit1_DCS');
 define('DB_PREFIX', 'oc_');
